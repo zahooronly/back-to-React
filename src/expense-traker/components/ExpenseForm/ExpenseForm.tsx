1375cd099232aa836b4dbf70a93a2afff8e822c7
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-19 12:13:23 +0500

    Expense Form updated as Validation done with zod and react hook form

--- src/expense-traker/components/ExpenseForm/ExpenseForm.tsx
+++ src/expense-traker/components/ExpenseForm/ExpenseForm.tsx
@@ -1,32 +1,79 @@
-import React from "react";
-import { categories } from "../../../App";
+import { number, z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import categories from "../../categories";
+const schema = z.object({
+  description: z.string().min(3).max(50),
+  amount: z.number().min(0.01).max(100_000_000),
+  category: z.enum(categories),
+});
 
-const ExpenseForm = () => {
+type ExpenseDataForm = z.infer<typeof schema>;
+interface Props {
+  onFormSubmit: (data: ExpenseDataForm) => void;
+}
+
+const ExpenseForm = ({ onFormSubmit }: Props) => {
+  const {
+    register,
+    reset,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ExpenseDataForm>({ resolver: zodResolver(schema) });
   return (
-    <form className="mb-3">
+    <form
+      className="mb-3"
+      onSubmit={handleSubmit((data) => {
+        onFormSubmit(data), reset();
+      })}
+    >
       <div className="mb-3">
-        <label htmlFor="Description" className="form-label">
+        <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input type="text" className="form-control" id="Description" />
+        <input
+          {...register("description")}
+          type="text"
+          className="form-control"
+          id="description"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="Amount" className="form-label">
+        <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input type="number" className="form-control" id="Amount" />
+        <input
+          type="number"
+          className="form-control"
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select name="category" id="category" className="form-select">
+        <select
+          {...register("category")}
+          name="category"
+          id="category"
+          className="form-select"
+        >
           {categories.map((category) => (
             <option key={category} value={category}>
               {category}
             </option>
           ))}
         </select>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
       </div>
       <button className="btn btn-primary">Submit</button>
     </form>
