c69413a3c8f38e2999b92ff79b2169c7af559f0d
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-19 10:17:09 +0500

    Expense Traker Project

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,44 @@ import Alert from "./components/Alert/Alert";
 import Button from "./components/Button/Button";
 import Like from "./components/Like/Like";
 import ListGroup from "./components/ListGroup/ListGroup";
+import ExpenseList from "./expense-traker/components/ExpenseList/ExpenseList";
 
 function App() {
-  let americanStates = [
-    "Alabama",
-    "Alaska",
-    "Arizona",
-    "Arkansas",
-    "California",
-    "Colorado",
-    "Connecticut",
-    "Delaware",
-    "Florida",
-    "Georgia",
+  // let americanStates = [
+  //   "Alabama",
+  //   "Alaska",
+  //   "Arizona",
+  //   "Arkansas",
+  //   "California",
+  //   "Colorado",
+  //   "Connecticut",
+  //   "Delaware",
+  //   "Florida",
+  //   "Georgia",
+  // ];
+  // americanStates = [];
+  // const onSelectHandler = (item: string) => {
+  //   console.log(item);
+  // };
+  // const [visibility, setVisibility] = useState(false);
+
+  let expenses = [
+    { id: 1, description: "Buy a car", amount: 10000, category: "Car" },
+    { id: 2, description: "Buy a house", amount: 100000, category: "House" },
+    { id: 3, description: "Buy a bike", amount: 1000, category: "Bike" },
+    { id: 4, description: "Buy a laptop", amount: 100, category: "Laptop" },
+    { id: 5, description: "Buy a phone", amount: 100, category: "Phone" },
+    // create five more objects of laptop category
+    { id: 6, description: "Buy a laptop", amount: 100, category: "Laptop" },
+    { id: 7, description: "Buy a laptop", amount: 100, category: "Laptop" },
+    { id: 8, description: "Buy a laptop", amount: 100, category: "Laptop" },
+    { id: 9, description: "Buy a laptop", amount: 100, category: "Laptop" },
+    { id: 10, description: "Buy a laptop", amount: 100, category: "Laptop" },
   ];
-  americanStates = [];
-  const onSelectHandler = (item: string) => {
-    console.log(item);
-  };
-  const [visibility, setVisibility] = useState(false);
+
   return (
     <div>
-      <ListGroup
+      {/* <ListGroup
         americanStates={americanStates}
         onSelectItem={onSelectHandler}
         heading="Cities"
@@ -36,7 +52,12 @@ function App() {
         </Alert>
       )}
       <Button clickFunc={() => setVisibility(true)}>Click here</Button>
-      <Like clickOnLike={() => console.log("Clicked")} />
+      <Like clickOnLike={() => console.log("Clicked")} /> */}
+
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      />
     </div>
   );
 }
---
+++ src/expense-traker/components/ExpenseList/ExpenseList.tsx
@@ -0,0 +1,47 @@
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
+  return (
+    <div>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Amount</th>
+            <th>Category</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={expense.id}>
+              <td>{expense.description}</td>
+              <td>{expense.amount}</td>
+              <td>{expense.category}</td>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => onDelete(expense.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ExpenseList;
