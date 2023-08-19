34157335b2977a6baf118b2e609dfbcd042b88f9
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-08-19 12:16:44 +0500

    code added to App_Old file

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,7 @@
-import { useState } from "react";
-import Alert from "./components/Alert/Alert";
-import Button from "./components/Button/Button";
-import Like from "./components/Like/Like";
-import ListGroup from "./components/ListGroup/ListGroup";
-import ExpenseList from "./expense-traker/components/ExpenseList/ExpenseList";
-import ExpenseFilter from "./expense-traker/components/ExpenseFilter/ExpenseFilter";
-import ExpenseForm from "./expense-traker/components/ExpenseForm/ExpenseForm";
-import categories from "./expense-traker/categories";
-function App() {
-  // let americanStates = [
-  //   "Alabama",
-  //   "Alaska",
-  //   "Arizona",
-  //   "Arkansas",
-  //   "California",
-  //   "Colorado",
-  //   "Connecticut",
-  //   "Delaware",
-  //   "Florida",
-  //   "Georgia",
-  // ];
-  // americanStates = [];
-  // const onSelectHandler = (item: string) => {
-  //   console.log(item);
-  // };
-  // const [visibility, setVisibility] = useState(false);
-  const [selected, setSelected] = useState("");
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "Buy a car", amount: 10000, category: "Car" },
-    { id: 2, description: "Buy a house", amount: 100000, category: "House" },
-    { id: 3, description: "Buy a bike", amount: 1000, category: "Bike" },
-    { id: 4, description: "Buy a laptop", amount: 100, category: "Laptop" },
-    { id: 5, description: "Buy a phone", amount: 100, category: "Phone" },
-    { id: 10, description: "Buy a laptop", amount: 100, category: "Laptop" },
-  ]);
+import React from "react";
 
-  const visibleExpenses = selected
-    ? expenses.filter((expense) => expense.category === selected)
-    : expenses;
+const App = () => {
+  return <div>App</div>;
+};
 
-  return (
-    <div>
-      {/* <ListGroup
-        americanStates={americanStates}
-        onSelectItem={onSelectHandler}
-        heading="Cities"
-        // Alert
-      />
-      {visibility && (
-        <Alert onClose={() => setVisibility(false)}>
-          <h1>Alert Component</h1>
-        </Alert>
-      )}
-      <Button clickFunc={() => setVisibility(true)}>Click here</Button>
-      <Like clickOnLike={() => console.log("Clicked")} /> */}
-      <ExpenseForm
-        onFormSubmit={(expense) =>
-          setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-        }
-      />
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={(category) => setSelected(category)} />
-      </div>
-      <ExpenseList
-        // expenses={expenses}
-        expenses={visibleExpenses}
-        onDelete={(id) =>
-          setExpenses(expenses.filter((expense) => expense.id !== id))
-        }
-      />
-    </div>
-  );
-}
 export default App;
