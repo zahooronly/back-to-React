0e5431a3e4ab6272bd90a9692282e4b4fb15a3c5
Zahoor Ahmad <i.am.zahoor.a@outlook.com>
2023-09-01 15:56:02 +0500

    main and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <Header/>
+  </div>;
 };
 
 export default App;
